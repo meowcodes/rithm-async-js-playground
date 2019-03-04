e81da31ee34506683f6c56e6e2072dfa72cebbf6
meowcodes <meowcodes@gmail.com>
2019-03-04 11:06:55 -0800

    make number facts requests

---
+++ async.js
@@ -0,0 +1,22 @@
+const BASE_URL = "http://numbersapi.com"
+
+
+$(document).ready(async function(){
+    let resp = await $.ajax({
+        method: "GET",
+        url: `${BASE_URL}/3,7,9,13?json`
+    });
+    
+    console.log(resp)
+
+    let multresp = []
+
+    for(let i = 0; i < 4; i++){
+        multresp.push(await $.ajax({
+            method: "GET",
+            url: `${BASE_URL}/27?json`
+        }));
+    }
+
+    console.log(multresp)
+})
