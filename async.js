d51d3e56da53db1c17f73916af21eb184491b07f
Stéphane Dryefus <floatingrock@gmail.com>
2019-03-04 11:20:55 -0800

    Add card draw function.

--- async.js
+++ async.js
@@ -1,7 +1,9 @@
-const BASE_URL = "http://numbersapi.com"
 
 
 $(document).ready(async function(){
+
+    const BASE_URL = "http://numbersapi.com"
+
     let resp = await $.ajax({
         method: "GET",
         url: `${BASE_URL}/3,7,9,13?json`
@@ -19,4 +21,24 @@ $(document).ready(async function(){
     }
 
     console.log(multresp)
+})
+
+
+$(document).ready(async function(){
+
+    const BASE_URL = "https://deckofcardsapi.com/api/deck/"
+    
+    let resp = await $.ajax({
+        method: "GET",
+        url: `${BASE_URL}new/shuffle/?deck_count=1`
+    });
+    
+    let deckId = resp.deck_id;
+
+    let card = await $.ajax({
+        method: "GET",
+        url: `${BASE_URL}${deckId}/draw/?count=1`
+    })
+    console.log(`${card.cards[0].value} of ${card.cards[0].suit}`)
+
 })
